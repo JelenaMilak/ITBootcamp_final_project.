6e4d47ed9409717ccaccdba9ed81961e102f5d76
JelenaMilak <milakovicjelena@yahoo.com>
2022-10-07 11:58:53 +0200

    Commit 8.

--- src/main/java/ProfilePage.java
+++ src/main/java/ProfilePage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ProfilePage extends BasePage {
     Faker faker = new Faker();
+    private By cityManu = By.xpath("//*[@id='list-item-162-6']/div");
     private By email = By.xpath("//*[@id='app']/div[1]/main/div/div[2]/div/div/div[2]/span/form/div/div/div[1]");
     private By name = By.xpath("//*[@id='name']");
     private By phone = By.xpath("//*[@id='phone']");
@@ -14,6 +15,7 @@ public class ProfilePage extends BasePage {
     private By twiter = By.xpath("//*[@id='urlTwitter']");
     private By gitHub = By.xpath("//*[@id='urlGitHub']");
     private By saveBtn = By.xpath("//*[@id='app']/div[1]/main/div/div[2]/div/div/div[2]/span/form/div/div/div[8]/button");
+    private By arrow = By.xpath("//*[@id='app']/div[1]/main/div/div[2]/div/div/div[2]/span/form/div/div/div[4]/span/div/div/div[1]/div[1]/div[2]/div/i");
 
     public ProfilePage(WebDriver driver, WebDriverWait driverWait) {
         super(driver, driverWait);
@@ -21,6 +23,22 @@ public class ProfilePage extends BasePage {
 
     public void setFaker(Faker faker) {
         this.faker = faker;
+    }
+
+    public WebElement getCityManu() {
+        return getDriver().findElement(cityManu);
+    }
+
+    public void setCityManu(By cityManu) {
+        this.cityManu = cityManu;
+    }
+
+    public WebElement getArrow() {
+        return getDriver().findElement(arrow);
+    }
+
+    public void setArrow(By arrow) {
+        this.arrow = arrow;
     }
 
     public void setEmail(By email) {
@@ -87,33 +105,46 @@ public class ProfilePage extends BasePage {
         return getDriver().findElement(saveBtn);
     }
 
-    public void editFakerProfile(String name, String phone, String city, String contry, String twiter, String gitHub) {
+    public void editFakerProfile(String name, String phone, String city, String contry, String twiter, String gitHub, String arrow) throws InterruptedException {
 
         name = faker.internet().domainName();
         phone = faker.phoneNumber().cellPhone();
-        city = faker.address().cityName();
+
+        city = faker.address().city();
+
         contry = faker.address().country();
         twiter = faker.internet().url();
-        gitHub = faker.internet().ipV4Address();
+        gitHub = faker.internet().url();
+
 
 
         getName().click();
         getName().sendKeys(name);
+        Thread.sleep(5000);
+
 
         getPhone().click();
         getPhone().sendKeys(phone);
+        Thread.sleep(5000);
 
-        getCity().click();
-        getCity().sendKeys(city);
+        getArrow().click();
+        //getCity().click();
+        getCityManu().click();
+        //getCity().sendKeys();
+        Thread.sleep(5000);
+
 
         getContry().click();
         getContry().sendKeys(contry);
+        Thread.sleep(5000);
 
         getTwiter().click();
-        getTwiter().sendKeys(twiter);
+        getTwiter().sendKeys("https://"+ twiter);
+        Thread.sleep(5000);
 
         getGitHub().click();
-        getGitHub().sendKeys(gitHub);
+        getGitHub().sendKeys("https://"+gitHub);
+        Thread.sleep(5000);
 
         getSaveBtn().click();
 
--- src/test/java/ProfileTest.java
+++ src/test/java/ProfileTest.java
@@ -1,12 +1,15 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class ProfileTest extends BaseTest{
     @Test
-    public void editProfileTest(){
+    public void editProfileTest() throws InterruptedException {
         wait = new WebDriverWait(driver, Duration.ofSeconds(60));
         homePage.getSingUpBtn().click();
         singUpPage.SingUp();
@@ -16,8 +19,13 @@ public class ProfileTest extends BaseTest{
 
         homePage.getProfileBtn().click();
         wait.until(ExpectedConditions.elementToBeClickable(profilePage.getContry()));
+        Thread.sleep(500);
 
-        profilePage.editFakerProfile("name","phone","city","contry","twiter","github");
+        profilePage.editFakerProfile("name","phone","city","contry","twiter","github","arrow");
+
+        WebElement succMsg = driver.findElement(By.xpath("//*[@id='app']/div[1]/main/div/div[2]/div/div/div[4]/div/div/div/div"));
+        String expectedResult ="Profile saved successfuly";
+        Assert.assertTrue(succMsg.getText().contains(expectedResult));
 
 
 
