bf004d74b22748c063e4ca21f4c15ee6378616ff
JelenaMilak <milakovicjelena@yahoo.com>
2022-10-08 23:52:30 +0200

    Commit 11.

--- src/main/java/AdminPage.java
+++ src/main/java/AdminPage.java
@@ -4,8 +4,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AdminPage extends BasePage {
+
+    private By myCity = (By.xpath("//*[@id='app']/div[5]/div/div/div[3]/button[2]/span"));
+
+    private By search = By.id("search");
     private By editBitn = By.xpath("//*[@id='edit']/span");
-    private By saveBtn = By.xpath("//*[@id='app']/div[5]/div/div/div[3]/button[2]");
+    private By saveBtn = By.xpath("/html/body/div/div[5]/div/div/div[3]/button[2]");
     private By newItem = By.xpath("//*[@id='app']/div[1]/main/div/div[2]/div/div[1]/div[1]/div[3]/form/div[1]/button/span");
     private By cityName = By.xpath("//*[@id='name']");
     private By adminBtn = By.xpath("//*[@id='app']/div[1]/div/header/div/div[3]/a[1]");
@@ -18,7 +22,12 @@ public class AdminPage extends BasePage {
     public AdminPage(WebDriver driver, WebDriverWait driverWait) {
         super(driver, driverWait);
     }
-    public WebElement getEditBtn (){
+
+    public WebElement getMyCity() {
+        return getDriver().findElement(myCity);
+    }
+
+    public WebElement getEditBtn() {
         return getDriver().findElement(editBitn);
     }
 
@@ -65,8 +74,17 @@ public class AdminPage extends BasePage {
     public WebElement getRightArrow() {
         return getDriver().findElement(rightArrow);
     }
-    public void setCityName(String cityName){
-        cityName="Jelena";
+
+    public WebElement getSearch() {
+        return getDriver().findElement(search);
     }
+
+    public void searchCity() {
+        getSearch().click();
+        getSearch().sendKeys("Morinj editedhophop");
+
+
+    }
+
 
 }
--- src/test/java/AdminTests.java
+++ src/test/java/AdminTests.java
@@ -2,6 +2,7 @@
 
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -35,7 +36,7 @@ public class AdminTests extends BaseTest {
         adminPage.getCityBtn().click();
         adminPage.getNewItem().click();
 
-        adminPage.getCityName().sendKeys("opop");
+        adminPage.getCityName().sendKeys("Luj");
         adminPage.getSaveBtn().click();
 
         wait.until(ExpectedConditions.visibilityOf(adminPage.getSaveBtn()));
@@ -53,29 +54,41 @@ public class AdminTests extends BaseTest {
         loginPage.login("admin@admin.com", "12345");
         homePage.getAdminBtn().click();
         adminPage.getCityBtn().click();
-
         adminPage.getEditBtn().click();
-        WebElement myCity = driver.findElement(By.xpath("//*[@id='app']/div[5]/div/div/div[3]/button[2]/span"));
 
-        WebElement editMyCity = driver.findElement(By.xpath("//*[@id='name']"));
-        editMyCity.click();
-        editMyCity.clear();
-        editMyCity.sendKeys("opop");
+
+        adminPage.getCityName().sendKeys(Keys.CONTROL + "A", Keys.DELETE);
+        adminPage.getCityName().sendKeys("Luj-edited");
+
+        //wait.until(ExpectedConditions.visibilityOf(adminPage.getSaveBtn()));
         wait.until(ExpectedConditions.textToBe(By.xpath("//*[@id='app']/div[1]/main/div/div[2]/div/div[3]/div/div/div/div/div[1]"),
                 "Saved successfully\nCLOSE"));
-        WebElement messageBox = driver.findElement(By.xpath("//*[@id='app']/div[1]/main/div/div[2]/div/div[3]/div/div/div/div/div[1]"));
+        WebElement messageBox = driver.findElement(By.xpath("//*[@id='app']/div[5]/div/div/div[3]/button[2]/span"));
         Assert.assertTrue(messageBox.getText().contains("Saved successfully"));
-        profilePage.getSaveBtn();
+
+        profilePage.getSaveBtn().click();
     }
+
 
     @Test
-    protected void searchCity() {
+    protected void searchCity() throws InterruptedException {
+        loginPage.getLoginBtn1().click();
+        loginPage.login("admin@admin.com", "12345");
+        homePage.getAdminBtn().click();
+        adminPage.getCityBtn().click();
+
+        adminPage.searchCity();
+
+        Thread.sleep(7000);
+        String expected = driver.findElement(By.xpath(" //*[@id='app']/div[1]/main/div/div[2]/div/div[1]/div[2]/table/tbody/tr/td")).getText();
+        Assert.assertTrue(adminPage.getSearch().getText().contains(expected));
 
 
     }
-
-
 }
+
+
+
 
 
 
